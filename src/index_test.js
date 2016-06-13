c3d991904291510864e35c403065da3f4390e26c
Vladimir Novick <vnovick@gmail.com>
2016-06-13 19:35:24 +0300

    renaming tests for readability

--- src/index_test.js
+++ src/index_test.js
@@ -6,31 +6,31 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import { Provider } from 'react-redux';
 import appStore from './appStore';
-describe('Test environment is up', ()=> {
+describe('(index_test.js) - Test environment is up', ()=> {
   jsdom()
 
-  it('has document', () => {
+  it('jsDom defined with correct document', () => {
     var div = document.createElement('div')
     expect(div.nodeName).toBe('DIV')
   })
-  it('has Editor component', () => {
+  it('Public Editor component is imported correctly', () => {
     expect(Editor).toExist();
   })
 
 })
 
-describe('Basic Editor setup', ()=>{
+describe('(index_test.js) - Basic Public Editor setup', ()=>{
   let wrapper;
   beforeEach(()=>{
     wrapper = shallow(<Editor/>);
   })
-  it('Editor has proper className of StewieEditor', ()=>{
+  it('Public Editor renders StewieEditor with proper className of "StewieEditor"', ()=>{
     expect(wrapper.find('.StewieEditor')).toExist();
   })
-  it('Editor is wrapped by Provider', ()=>{
+  it('Stewie Editor is exposed wrapped in Provider', ()=>{
     expect(wrapper.name()).toEqual('Provider')
   })
-  it('Editor is passed correct appStore',()=>{
+  it('Provider wrapper gets correct appStore',()=>{
     expect(wrapper.props().store).toEqual(appStore)
   })
 })
